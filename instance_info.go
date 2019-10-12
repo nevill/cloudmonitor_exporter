38f35ccb855d4c1dca8fd9f0b55049878200695a
Nevill <nevill.dutt@gmail.com>
2019-10-12 22:50:59 +0800

    Collect all the rds instances info based on pagination api

--- instance_info.go
+++ instance_info.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/rds"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/slb"
 	"github.com/prometheus/client_golang/prometheus"
@@ -18,9 +19,9 @@ var instance_info = prometheus.NewGaugeVec(
 
 func collectSLBInfo() {
 	client := newSLBClient()
-	request := slb.CreateDescribeLoadBalancersRequest()
-	request.Scheme = "https"
-	response, err := client.DescribeLoadBalancers(request)
+	req := slb.CreateDescribeLoadBalancersRequest()
+	req.Scheme = "https"
+	response, err := client.DescribeLoadBalancers(req)
 	if err != nil {
 		log.Println("Get SLB response error: ", err)
 	} else {
@@ -31,16 +32,28 @@ func collectSLBInfo() {
 }
 
 func collectRDSInfo() {
+	// 100 is the maximum number of items can be returned in One request
+	const pageSize int = 100
+
 	client := newRDSClient()
-	request := rds.CreateDescribeDBInstancesRequest()
-	request.Scheme = "https"
-	response, err := client.DescribeDBInstances(request)
-	if err != nil {
-		log.Println("Get RDS response error: ", err)
-	} else {
-		for _, db := range response.Items.DBInstance {
-			//TODO can define which field will be set as name in Config file
-			instance_info.WithLabelValues("rds", db.DBInstanceId, db.DBInstanceDescription).Set(1)
+	req := rds.CreateDescribeDBInstancesRequest()
+	req.Scheme = "https"
+	req.PageSize = requests.NewInteger(pageSize)
+
+	for hasNextPage, pageNum := true, 1; hasNextPage != false; pageNum++ {
+		req.PageNumber = requests.NewInteger(pageNum)
+		response, err := client.DescribeDBInstances(req)
+		if response.PageRecordCount < pageSize {
+			hasNextPage = false
+		}
+
+		if err != nil {
+			log.Println("Get RDS response error: ", err)
+		} else {
+			for _, db := range response.Items.DBInstance {
+				//TODO can define which field will be set as name in Config file
+				instance_info.WithLabelValues("rds", db.DBInstanceId, db.DBInstanceDescription).Set(1)
+			}
 		}
 	}
 }
